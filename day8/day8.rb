4e1ec5cac6ac8f753164e1aea3f7d1dad84f2804
Karsten Huneycutt <kph@pobox.com>
2022-01-03 06:34:39 -0500

    day8

---
+++ day8/day8.rb
@@ -0,0 +1,9 @@
+strings = File.readlines("/Users/kph/Stuff/aoc2015/day8/input.txt").map(&:strip)
+
+code = strings.map(&:length).reduce(0, &:+)
+memory = strings.map { |s| eval(s) }.map(&:length).reduce(0, &:+)
+
+puts "Difference is #{code-memory}"
+
+escaped = strings.map(&:inspect).map(&:length).reduce(0, &:+)
+puts "Difference is #{escaped-code}"
