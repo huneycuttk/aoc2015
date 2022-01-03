914ca7929d5066a34e91081dce5866f8bd39caea
Karsten Huneycutt <kph@pobox.com>
2022-01-03 03:10:06 -0500

    day2

---
+++ day2/day2.rb
@@ -0,0 +1,11 @@
+dimensions = File.readlines("/Users/kph/Stuff/aoc2015/day2/input.txt")
+
+paper, ribbon = dimensions.reduce([0,0]) do |(paper, ribbon), dim|
+  (l,w,h) = dim.split("x").map(&:to_i)
+  paper += 2*l*w + 2*w*h + 2*l*h + [l,w,h].sort[0..1].reduce(1, &:*)
+  ribbon += l*w*h + [l,w,h].sort[0..1].reduce(0) { |r, s| r + 2*s }
+  [paper, ribbon]
+end
+
+# paper is 1606483
+puts "paper is #{paper}, ribbon is #{ribbon}"
